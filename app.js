0766005db8a9cba36a73133db9d71e1764efdb2e
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-07-17 11:06:53 +0700

    Chagne for loop to forEach

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let items = [];
+let posts = [];
 
 const app = express();
 
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home" ,{ startingContent: homeStartingContent });
+  console.log(posts);
+  res.render("home" ,{ startingContent: homeStartingContent , post: posts});
 });
 
 app.get("/about", (req, res) => {
@@ -34,8 +35,14 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  let item = req.body.newPost;
-  console.log(item);
+  const collectData = {
+    Title: req.body.newPost,
+    Post: req.body.postBody 
+  };
+
+  posts.push(collectData);
+
+  res.redirect("/");
 });
 
 
