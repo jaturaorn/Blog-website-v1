620a91aef9d3a336f083223134b5ffe0ef21a546
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-07-17 12:23:38 +0700

    install lodash , routes to post content

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,7 +19,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log(posts);
   res.render("home" ,{ startingContent: homeStartingContent , post: posts});
 });
 
@@ -45,7 +45,22 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/post/:Title", (req, res) => {
+  const requestTitle = _.lowerCase(req.params.Title);
 
+  posts.forEach((post) => {
+    let storedTitle = _.lowerCase(post.Title);
+
+    if (storedTitle === requestTitle) {
+      res.render("post", {  
+        title: post.Title , 
+        content: post.Post
+      });
+    }
+  })
+
+  
+});
 
 
 app.listen(3000, function() {
